79a86fb949a760266ac96f51e414b0d208a83efa
Singularity1984 <lol.2test.73@bk.ru>
2025-03-08 23:56:20 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function createCommetn() {
+    const name = document.getElementById('name').value;
+    const comment = document.getElementById('comment').value;
+    const comments = document.getElementById('comments');
+  
+    const commentHTML = `
+      <div class="comment">
+        <h2>${name}:</h2>
+        <p class="margin10px">
+         ${comment}
+        </p>
+       </div>
+    `;
+  
+    comments.innerHTML += commentHTML;
+}
