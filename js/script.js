a11ca5641e81e70742487cd78e931e85c3256108
Singularity1984 <lol.2test.73@bk.ru>
2025-03-17 08:46:01 +0400

    Update script.js
    Signed-off-by: Singularity1984 <lol.2test.73@bk.ru>

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@ function createCommetn() {
     const name = document.getElementById('name').value;
     const comment = document.getElementById('comment').value;
     const comments = document.getElementById('comments');
-  
+    const stars = document.getElementById('stars').value;
+    let star =`<div class="star"></div>`
+
+    for(let i=0; i<stars-1; i++){
+        star+=`<div class="star"></div>`;
+    }
     const commentHTML = `
       <div class="comment">
         <h2>${name}:</h2>
         <p class="margin10px">
          ${comment}
         </p>
+        <div class="stars">${star}</div>
        </div>
     `;
   
